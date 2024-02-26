d2fbcb72e8a81e79955832007366d54297fd3f76
kliuc <115437942+kliuc@users.noreply.github.com>
2024-02-26 01:11:28 -0800

    Add files via upload

---
+++ losses.py
@@ -0,0 +1,22 @@
+### Losses
+import numpy as np
+class MSE:
+    def loss(self, predictions, y):
+
+        l = np.mean((y - predictions)**2)
+        return l
+    def loss_gradient(self, predictions, y):
+        grad = -2*(y - predictions)/y.shape[1]
+        return grad.T
+    
+class Cross_Entropy:
+    
+    def loss(self, predictions, y):
+        #compute here the cross entropy loss l, you may want to add 1e-6 to the argument of the logarithm for numercal stability
+        l = -np.sum(y * np.log(predictions + 1e-6)) / y.shape[1]
+        return l
+    
+    def loss_gradient(self, predictions, y):
+        #compute here the cross entropy loss l as computed before, the result, grad, should be of dimension n x m, where m is the number of points in the batch
+        grad = -(y / (predictions + 1e-6)) / y.shape[1]
+        return grad.T
